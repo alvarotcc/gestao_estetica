6c5ad2652504634d3a146baa07708c8e7dc2c728
alvarotcc <alvarotcc01@gmail.com>
2025-09-04 14:38:45 -0300

    Corrigindo o basename para GitHub Pages e gerando novo build

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/gestao_estetica">
         <Routes>
           <Route path="/" element={
             <MainLayout>
